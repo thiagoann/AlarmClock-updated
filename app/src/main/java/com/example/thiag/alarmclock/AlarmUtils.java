3755e96bea1c80a31efe8fdd23d8a78aab1ea68b
Thiago Martins <thiagoann@gmail.com>
2018-04-05 10:31:04 -0300

    Consertado os erros. Dando erro no Event Log, o que não deixa abrir no Emulator.

--- app/src/main/java/com/example/thiag/alarmclock/AlarmUtils.java
+++ app/src/main/java/com/example/thiag/alarmclock/AlarmUtils.java
@@ -1,10 +1,14 @@
 package com.example.thiag.alarmclock;
 
+import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+
+import java.util.Calendar;
+import java.util.concurrent.atomic.AtomicLongArray;
 
 
 /**
@@ -18,28 +22,31 @@ public class AlarmUtils {
     //Set an alarm for the day/time chosen by the user
     public static void schedule(Context context, Intent intent, long triggerAtMillis) {
         PendingIntent p = PendingIntent.getBroadcast(context, 1, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        AlarmManager alarmclock = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
-        alarmclock.set(AlarmManager.RTC_WAKEUP, triggerAtMillis,p);
+        AlarmManager alarmClock = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarmClock.set(AlarmManager.RTC_WAKEUP, triggerAtMillis, p);
         Log.d("MyAlarmClock", " Alarm set! ");
 
     }
 
 
     //Set an alarm with the repeat option
-    public  static void scheduleRepeat(Context context, Intent intent, long triggerAtMillis, long intervalMillis){
-    PendingIntent p = PendingIntent.getBroadcast(context, 1, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        AlarmManager alarmclock = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
-        alarmclock.setInexactRepeating(AlarmManager.RTC_WAKEUP, triggerAtMillis, intervalMillis, p);
+    public static void scheduleRepeat(Context context, Intent intent, long triggerAtMillis, long intervalMillis) {
+        PendingIntent p = PendingIntent.getBroadcast(context, 1, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        AlarmManager alarmClock = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarmClock.setInexactRepeating(AlarmManager.RTC_WAKEUP, triggerAtMillis, intervalMillis, p);
         Log.d("MyAlarmClock", " Alarm is going to repeat everyday. ");
 
     }
 
     //Dismiss an alarm
-    public static void dismiss(Context context, Intent intent){
-        AlarmManager alarmclock = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+    public static void dismiss(Context context, Intent intent) {
+        AlarmManager alarmClock = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         PendingIntent p = PendingIntent.getBroadcast(context, 1, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        alarmclock.cancel(p);
+        alarmClock.cancel(p);
         Log.d("MyAlarmClock", " Alarm dismissed. ");
 
     }
+
+
 }
+
--- app/src/main/java/com/example/thiag/alarmclock/MainActivity.java
+++ app/src/main/java/com/example/thiag/alarmclock/MainActivity.java
@@ -2,20 +2,21 @@ package com.example.thiag.alarmclock;
 
 import android.app.AlarmManager;
 import android.app.NotificationChannel;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TimePicker;
 import android.widget.Toast;
-
-
 import java.time.Clock;
 import java.util.Calendar;
-
 import static java.util.Calendar.AM;
 import static java.util.Calendar.HOUR_OF_DAY;
 
@@ -33,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Notifications.createChannel(this);
+//        Notifications.createChannel(this);
 
 
         time_Alarm = findViewById(R.id.time_Alarm);
@@ -49,37 +50,46 @@ public class MainActivity extends AppCompatActivity {
 
 
         btn_set.setOnClickListener(new View.OnClickListener() {
-            Boolean check = false;
+//            Boolean check = false;
 
             @Override
             public void onClick(View view) {
-                if (time_Alarm.equals("")) {
-                    check = false;
-                    Toast.makeText(getApplicationContext(), " What time do you want to wake up? ", Toast.LENGTH_LONG);
-
-                }
+                Toast myToast;
+                myToast = Toast.makeText(getApplicationContext(), " What time do you want to wake up? ", Toast.LENGTH_LONG);
+//                if (time_Alarm.equals("")) {
+//                    check = false;
+//                    Toast.makeText(getApplicationContext(), " What time do you want to wake up? ", Toast.LENGTH_LONG);
+//
+//                }
+            myToast.show();
+                onClickSet();
             }
         });
 
         btn_dismiss.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), " Alarm dismissed. ", Toast.LENGTH_SHORT);
-            }
+                Toast.makeText(getApplicationContext(), " Alarm dismissed. ", Toast.LENGTH_SHORT).show();
+                onClickDismiss();
+            };
         });
 
         btn_repeat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), " Alarm set to repeat every day. ", Toast.LENGTH_SHORT);
+                Toast.makeText(getApplicationContext(), " Alarm set to repeat every day. ", Toast.LENGTH_SHORT).show();
+                onClickRepeat();
             }
         });
 
         btn_snooze.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), " Alarm snoozed for 10 minutes. ", Toast.LENGTH_SHORT);
+                Toast.makeText(getApplicationContext(), " Alarm snoozed for 10 minutes. ", Toast.LENGTH_SHORT).show();
+                onClickSnooze();
             }
+
+
         });
 
     }
@@ -89,6 +99,7 @@ public class MainActivity extends AppCompatActivity {
         int hour = time_Alarm.getCurrentHour();
         int min = time_Alarm.getCurrentMinute();
         showTime(hour, min);
+
     }
 
     public void showTime(int hour, int min) {
@@ -111,13 +122,15 @@ public class MainActivity extends AppCompatActivity {
     public long getTime() {
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(System.currentTimeMillis());
-        calendar.set(Calendar.HOUR_OF_DAY, 9);
-        calendar.set(Calendar.MINUTE, 00);
+        calendar.set(Calendar.HOUR, 9);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+
         long time = calendar.getTimeInMillis();
         return time;
     }
 
-    public void onClickSet(View view) {
+    public void onClickSet() {
         Intent intent = new Intent(WakeupThiagoReceiver.ACTION);
         //Set an alarm
         AlarmUtils.schedule(this, intent, getTime());
@@ -126,7 +139,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void onClickRepeat(View view) {
+    public void onClickRepeat() {
         Intent intent = new Intent(WakeupThiagoReceiver.ACTION);
         //Repeat an alarm everyday.
         AlarmUtils.scheduleRepeat(this, intent, getTime(), AlarmManager.INTERVAL_DAY);
@@ -134,17 +147,50 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(), " Alarm will repeat everyday. ", Toast.LENGTH_SHORT).show();
     }
 
-    public void onClickDismiss(View view) {
+    public void onClickDismiss() {
         Intent intent = new Intent(WakeupThiagoReceiver.ACTION);
         //Dismiss an alarm
         AlarmUtils.dismiss(this, intent);
         Toast.makeText(getApplicationContext(), " Alarm dismissed. ", Toast.LENGTH_SHORT).show();
 
 
+
+
     }
 
+    public void onClickSnooze(){
 
+        int snoozeCounter = 0;
+        snoozeCounter++;
+
+
+        Calendar calendar = Calendar.getInstance();
+
+
+        long currentTimeMillis = System.currentTimeMillis(); // declarou o tempo atual como long e pegou do sistema
+
+        long nextTimeMillis = (currentTimeMillis + 5 * DateUtils.MINUTE_IN_MILLIS); // declarou o tempo de soneca como proximo alarme ou next time +5 min
+
+        calendar.setTimeInMillis(nextTimeMillis);// Chamou a classe do calendario e setou o tempo do proximo alarme;
+
+        long nextSnoozeTime = calendar.getTimeInMillis(); // declra o tempo de soneca e pega da classe calendario o tempo
+
+        Intent intent = new Intent(getApplicationContext(),WakeupThiagoReceiver.class); //  Chama a tela do despertador
+
+        intent.putExtra("SNOOZE_COUNTER", snoozeCounter);// NÃO ESTA CLARO O CONTEXTO
+        PendingIntent pendingIntent = PendingIntent.getBroadcast// NÃO ESTA CLARO O CONTEXO DA CLASSE
+                (getApplicationContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);// Atualiza alguma coisa ...!!
+
+        AlarmManager alarmManager = (AlarmManager)getApplicationContext().getSystemService(Context.ALARM_SERVICE);
+
+        Log.d("snooze",  " Snooze set to 10 minutes " + calendar.getTime().toString());// registro de log no sistema
+
+        alarmManager.set(AlarmManager.RTC_WAKEUP, nextSnoozeTime, pendingIntent);// Setando o proximo alarme.
+
+        finish();
+    }
 }
+
 
 
 
